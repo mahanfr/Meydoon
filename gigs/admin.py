32f9cc687cb4b1a901aea5fcb5cfb5216a0c654a
Mahan <mahan.farzaneh@gmail.com>
2021-05-02 15:05:35 +0430

    admin fixed

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Gig, Category, Plan, Comment, ShowcaseImage, UserRate
+from .models import Gig, Category, SubCategory, Plan, Comment, ShowcaseImage, UserRate
 
 admin.site.register(Gig)
 admin.site.register(Category)
+admin.site.register(SubCategory)
 admin.site.register(Plan)
 admin.site.register(Comment)
 admin.site.register(ShowcaseImage)
