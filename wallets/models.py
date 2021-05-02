2c207f2a7c3903ccaac9a553b21bc4e7ba52e1aa
Mahan <mahan.farzaneh@gmail.com>
2021-05-02 14:21:57 +0430

    wallet model created

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Wallet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    amount = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return f'#{self.id}'
