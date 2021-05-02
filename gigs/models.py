d31b8da2dadd732aa226e70f8e4fc64834d452f3
Mahan <mahan.farzaneh@gmail.com>
2021-05-02 13:57:21 +0430

    gig models updated and wallets app has been created

--- gigs/models.py
+++ gigs/models.py
@@ -8,9 +8,10 @@ User = get_user_model()
 # This a model that hanndels advertisment for intrested user
 # to be shown to other users
 class Gig(models.Model):
-    title = models.CharField(max_length=25, blank=True, null=True)
+    title = models.CharField(max_length=50)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     category = models.ForeignKey('gigs.Category',on_delete=models.SET_NULL,null=True,blank=True)
+    sub_category = models.ForeignKey('gigs.SubCategory', on_delete=models.SET_NULL,null=True,blank=True)
     description = models.TextField()
     experience = models.PositiveSmallIntegerField(default=0)
 
@@ -22,20 +23,30 @@ class Gig(models.Model):
         return showcase[0].image
 
 
-
 # Every gig has a category and you can choose one 
 # from database of this model
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.name}'
+
+
+# Every gig has a category and you can choose one 
+# from database of this model
+class SubCategory(models.Model):
+    category = models.ForeignKey('gigs.Category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.category} - {self.name}'
 
 
 # every gig have one or more plans that can be
 # used to advertise different tier of services
 class Plan(models.Model):
     gig = models.ForeignKey('gigs.Gig',on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
     price = models.PositiveIntegerField(default=5000)
     feature_list = models.TextField()
 
