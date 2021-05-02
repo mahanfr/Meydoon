8effa292f15f59d4b323c6af12b7444ec58e21cd
Mahan <mahan.farzaneh@gmail.com>
2021-05-02 20:09:44 +0430

    Transaction model added / wallet last-withdraw-at added

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Wallet
 
-# Register your models here.
-admin.site.register(Wallet)
+class WalletConfig(admin.ModelAdmin):
+    readonly_fields=('user','amount')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Wallet,WalletConfig)
--- wallets/models.py
+++ wallets/models.py
@@ -1,11 +1,27 @@
 import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
 class Wallet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField('users.User', on_delete=models.CASCADE)
     amount = models.BigIntegerField(default=0)
+    last_withdraw_at = models.DateTimeField(null=True,blank=True)
 
     def __str__(self):
         return f'#{self.id}'
+
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    wallet = models.ForeignKey('wallets.Wallet', on_delete=models.PROTECT)
+    amount = models.BigIntegerField()
+    state = models.SmallIntegerField(default=1) # ckeck if transaction was seccessful
+    source = models.SmallIntegerField(default=0)
+    is_withdrawn = models.BooleanField(default=False) # check if the transaction is still dangerous to be removed
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'#{self.id}'
+
