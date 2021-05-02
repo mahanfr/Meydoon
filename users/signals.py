5411f5daa42ea326264a8a504b1a513cecb45587
Mahan <mahan.farzaneh@gmail.com>
2021-05-02 15:51:53 +0430

    wallet auto create added

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from .models import Profile
+from users.models import Profile
+from wallets.models import Wallet
 
 User = get_user_model()
 
@@ -10,3 +11,9 @@ def create_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+@receiver(post_save, sender=User)
+def create_wallet(sender,instance,created,**kwargs):
+    if created:
+        Wallet.objects.create(user=instance)
+    instance.wallet.save()
