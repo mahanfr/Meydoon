53781edc13b9babaf9bd784e6324e7828ae1571a
Mahan <mahan.farzaneh@gmail.com>
2021-05-07 20:27:13 +0430

    logo and categories added to nav-bar

--- pages/context_procesor.py
+++ pages/context_procesor.py
@@ -1,3 +1,5 @@
 import datetime
+from gigs.models import Category
+
 def context_procesor(request):
-    return{'year':datetime.datetime.now().year}
+    return{'year':datetime.datetime.now().year,'categories':Category.objects.all()}
