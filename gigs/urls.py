8d37426b15d64a7deee90199ca86efd5822dd32d
hamzehmahmoudi <hamzehmahmoodi7@gmail.com>
2021-05-11 18:24:40 +0430

    order view and form created

--- gigs/urls.py
+++ gigs/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.gig_index,name='gig_index'),
-    path('<int:gig_id>',views.gig_view,name='gig_info'),
-    path('create_gig', views.create_gig, name='create_gig'),
-    path('category/<int:category_id>', views.show_category, name='category')
+    path("", views.gig_index, name="gig_index"),
+    path("<int:gig_id>", views.gig_view, name="gig_info"),
+    path("create_gig", views.create_gig, name="create_gig"),
+    path("category/<int:category_id>", views.show_category, name="category"),
+    path("orders/", include("orders.urls")),
 ]
---
+++ orders/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from orders import views
+
+urlpatterns = [
+    path("", views.order, name="order"),
+]
