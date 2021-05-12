09544086e4315782b5e66555fbbec349a28f3183
hamzehmahmoudi <hamzehmahmoodi7@gmail.com>
2021-05-12 21:32:22 +0430

    add multiple image for gig

--- gigs/forms.py
+++ gigs/forms.py
@@ -1,38 +1,43 @@
 from django import forms
-from .models import Gig, Plan, ShowcaseImage,Comment
+from .models import Gig, Plan, ShowcaseImage, Comment
+
 
 class GigCreationForm(forms.ModelForm):
     """
-    Form for add Gig 
+    Form for add Gig
     """
+
     class Meta:
         model = Gig
-        fields = ['title','category', 'description', 'experience']
-        exclude = ['user']
+        fields = ["title", "category", "description", "experience"]
+        exclude = ["user"]
+
 
 class ShowcaseForm(forms.ModelForm):
     """
-    Form for add Gig 
+    Form for add Gig
     """
-    image = forms.ImageField(required=False)
     class Meta:
         model = ShowcaseImage
-        fields = ['image']
-        exclude = ['gig']
+        fields = ["image"]
+        exclude = ["gig"]
+
 
 class CommentForm(forms.ModelForm):
     body = forms.CharField(widget=forms.Textarea, max_length=400, required=False)
+
     class Meta:
         model = Comment
-        fields = ['title', 'body']
-        exclude = ['gig','user', 'is_aproved']
-                
+        fields = ["title", "body"]
+        exclude = ["gig", "user", "is_aproved"]
+
+
 class PlanForm(forms.ModelForm):
     """
-    Form for add Plans 
+    Form for add Plans
     """
+
     class Meta:
         model = Plan
-        fields = ['title', 'price', 'feature_list']
-        exclude = ['gig']                
-    
+        fields = ["title", "price", "feature_list"]
+        exclude = ["gig"]
