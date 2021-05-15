78d7a84f1fd2ca4daa851da2055d5d3f48c72b4d
Mahan <mahan.farzaneh@gmail.com>
2021-05-15 19:38:16 +0430

    Taliwind file updated

--- _Frontend/tailwind.config.js
+++ _Frontend/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors:{
+      brand:{
+        background: '#00061D',
+        card: '#5980DB',
+        gradientTop: '#4932C4',
+        gradientBottom: '#4076E2',
+        textColor1: '#EFEFEF',
+        textColor2: '#D4D4D4',
+      }
+    },
   },
   variants: {
     extend: {},
