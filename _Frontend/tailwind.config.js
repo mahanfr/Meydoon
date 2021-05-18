d15a1896ae03c17ac3e269ea0c37ee83147142e1
Mahan <mahan.farzaneh@gmail.com>
2021-05-19 00:21:11 +0430

    Ui migration to tailwind started

--- _Frontend/tailwind.config.js
+++ _Frontend/tailwind.config.js
@@ -2,16 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors:{
-      brand:{
-        background: '#00061D',
-        card: '#5980DB',
-        gradientTop: '#4932C4',
-        gradientBottom: '#4076E2',
-        textColor1: '#EFEFEF',
-        textColor2: '#D4D4D4',
-      }
+    extend: {
+      colors:{
+        brand:{
+          background: '#00061D',
+          card: '#5980DB',
+          gradientTop: '#4932C4',
+          gradientBottom: '#4076E2',
+          textColor1: '#EFEFEF',
+          textColor2: '#D4D4D4',
+        }
+      },
     },
   },
   variants: {
--- orders/urls.py
+++ orders/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path, include
 from orders import views
 
-urlpatterns = [path("", views.order, name="order"), path("/deliver", views.deliver, name="deliver")]
+urlpatterns = [path("", views.order, name="order"), path("deliver", views.deliver, name="deliver")]
