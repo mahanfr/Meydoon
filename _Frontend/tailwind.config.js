a3b3875ca49ad948a29c789c1318e21080999944
Mahan <mahan.farzaneh@gmail.com>
2021-05-23 21:24:49 +0430

    theme toggle and it's colors added

--- _Frontend/tailwind.config.js
+++ _Frontend/tailwind.config.js
@@ -1,16 +1,24 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors:{
         brand:{
-          background: '#00061D',
-          card: '#5980DB',
+          background:{
+            dark:'#050613',
+            DEFAULT:'#F5F5F5',
+          },
+          card:{
+            dark:'#0E1329',
+            DEFAULT:'#E4E8F2',
+          },
           gradientTop: '#4932C4',
           gradientBottom: '#4076E2',
           textColor1: '#EFEFEF',
           textColor2: '#D4D4D4',
+          textColor3: '#343434',
+          textColor4: '#707070',
         }
       },
     },
