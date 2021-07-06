cc773459af9ba193035b96c178350673b63c56ab
HamzehMahmoudi <46393208+HamzehMahmoudi@users.noreply.github.com>
2021-07-06 07:50:30 -0400

    fixed jquery problem and add order approve (#22)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.get_dashboard_mygigs, name="dashboard"),
     path("orders/", views.get_dashboard_orders, name="dashboard-orders"),
     path("profile-edit/", views.get_dashboard_profile_edit, name="dashboard-profile-edit"),
+    path("accept_orders/", views.set_order_status, name="set_status"),
     # path("comments/", views.register, name="dashboard-comments"),
     # path("messages/", views.register, name="dashboard-messages"),
     # path("myorders/", views.register, name="dashboard-myorders"),
